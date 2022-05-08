e34f635867f9490a62aaf384a845c417980029b6
Catherine McNeel <catherinemcneel@icloud.com>
2022-05-08 16:52:21 -0400

    heroku

--- Develop/server.js
+++ Develop/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://http://localhost:3001/', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001', {
   useNewUrlParser: true,
   useFindAndModify: true
 });
